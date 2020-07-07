6534c564b92ce33ec9638ce3f9b46685f9159b20
sridharyadav666 <kookatlasridharyadav@gmail.com>
2020-07-07 15:06:45 +0530

    my first version code completed

---
+++ Fourth-git/src/Fourth.java
@@ -0,0 +1,9 @@
+
+public class Fourth {
+
+	public static void main(String[] args) {
+		System.out.println("fourth one in Git");
+		System.out.println("WelCome to Git");
+	}
+
+}
--- SecondApplication-git/src/com/durgasoft/test/Test.java
+++ SecondApplication-git/src/com/durgasoft/test/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Git");
+		System.out.println("bye bye git");
+		System.out.println("good morning git");
 	}
 
 }
